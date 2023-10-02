cc067dcd5f5c837c1042304e2e0fe7671991b14d
tamiris-rtl <146030970+tamiris-rtl@users.noreply.github.com>
2023-10-02 13:47:19 -0300

    initial commit

---
+++ src/test/java/br/com/alura/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -0,0 +1,33 @@
+package br.com.alura.escola.aplicacao.aluno.matricular;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.escola.dominio.aluno.Aluno;
+import br.com.alura.escola.dominio.aluno.CPF;
+import br.com.alura.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+class MatricularAlunoTest {
+
+	@Test
+	void alunoDeveriaSerPersistido() {
+		// MOCK -> Mockito
+		RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
+		MatricularAluno useCase = new MatricularAluno(repositorio);
+		
+		MatricularAlunoDto dados = new MatricularAlunoDto(
+				"Tamiris",
+				"123.456.789-00", 
+				"tamiris@email.com");
+		useCase.executa(dados);
+		
+		Aluno encontrado = repositorio.buscarPorCPF(
+				new CPF("123.456.789-00"));
+		
+		assertEquals("Tamiris", encontrado.getNome());
+		assertEquals("123.456.789-00", encontrado.getCpf());
+		assertEquals("tamiris@email.com", encontrado.getEmail());
+	}
+
+}
